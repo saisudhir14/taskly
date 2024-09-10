c848b405d6660f73bf7b3af839c08d9d0323ad26
saisudhir14 <sudhirdontha@outlook.com>
2024-09-10 00:57:19 -0400

    standard reactNative component

--- App.tsx
+++ App.tsx
@@ -4,13 +4,8 @@ import { StyleSheet, Text, View, } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style = {{
-        borderBottomWidth:1,
-        borderBottomColor:"#1a759f",
-        paddingHorizontal:8,
-        paddingVertical:16
-      }}>
-      <Text style = {{fontSize: 32, fontWeight:"400"}}>Coffee</Text>
+      <View style = {styles.itemContainer}>
+      <Text style = {styles.itemText}>Coffee</Text>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -26,4 +21,15 @@ const styles = StyleSheet.create({
     //alignItems: "center",
     justifyContent: "center",
   },
+  itemContainer:{
+    borderBottomWidth:1,
+    borderBottomColor:"#1a759f",
+    paddingHorizontal:8,
+    paddingVertical:16
+  },
+  itemText:{
+      fontSize: 32, 
+      fontWeight:"400"
+    }
+
 });
